99773316964637b07330b59b3a4508e971a6da26
Deusexmortis <robbertmouthaan98@gmail.com>
2019-06-08 04:45:30 +0200

    small change to app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ client.commands = new enmap();
 fs.readdir('./events/', async (err, files) => {
     console.log('\n' + `---LOADING EVENTS---`);
     if (err) return console.error;
+    console.log("_____________________")
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
         const evt = require(`./events/${file}`);
@@ -20,6 +21,7 @@ fs.readdir('./events/', async (err, files) => {
 fs.readdir('./commands/', async (err, files) => {
     console.log('\n' + `---LOADING COMMANDS---`);
     if (err) return console.error;
+    console.log("_____________________")
     files.forEach(file => {
         if (!file.endsWith('.js')) return;
         let props = require(`./commands/${file}`);
