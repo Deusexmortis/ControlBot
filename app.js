8c2f8663d86be2ae12a96379b173fc58e51e9e24
Deusexmortis <robbertmouthaan98@gmail.com>
2019-06-08 18:58:27 +0200

    Added console log of js file and deleted additional catch (gave error)

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ client.commands = new enmap();
 
 fs.readdir('./Events/', async (err, files) => {
     console.log('\n' + `---LOADING EVENTS---`);
-    console.log(`de err is: ${err}`)
-    console.log(`de files is:  ${err}`)
     //if (err) return console.error;
     console.log("_____________________")
     files.forEach(file => {
+        console.log(`${file}`)
         if (!file.endsWith('.js')) return;
         const evt = require(`./events/${file}`);
         let evtName = file.split('.')[0];
         console.log(`${evtName}`);
         client.on(evtName, evt.bind(null, client));
     });
-}).catch(console.error);
+});
 
 fs.readdir('./commands/', async (err, files) => {
     console.log('\n' + `---LOADING COMMANDS---`);
